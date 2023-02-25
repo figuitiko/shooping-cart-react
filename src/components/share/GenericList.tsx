02508f06a9eb59d5c045560b7db0f37876786ae2
frank renato <frankfigao@gmail.com>
2023-02-24 20:09:30 -0600

    Adding generit list

--- src/components/share/GenericList.tsx
+++ src/components/share/GenericList.tsx
@@ -1,14 +1,42 @@
-interface ListProps<T> {
+export interface ItemProps<T> {
+ item?: T;
+  key?: string;
+}
+enum WrapperPossibles{
+  div,
+  span,
+  ul
+}
+export interface WrapperProps {
+  classToUse?: string;
+  children: React.ReactNode;
+  Possibles?: WrapperPossibles
+}
+interface ListProps<T> extends WrapperProps {
   items: T[];
-  renderItem: (item: T) => React.ReactNode;
-  Wrapper?: React.FC;
+  itemId: (item: T) => string;  
+  Component: React.FC<ItemProps<T>>;  
 }
 
-function GenericList<T>({ items, renderItem, Wrapper }: ListProps<T>) {
+const Wrapper = ({classToUse, children, Possibles }: WrapperProps) => {
+    switch (Possibles) {
+      case WrapperPossibles.div:
+        return <div className={classToUse}>{children}</div>;
+      case WrapperPossibles.span:
+        return <span className={classToUse}>{children}</span>;
+      case WrapperPossibles.ul:
+        return <li className={classToUse}>{children}</li>;
+      default:
+        return <div className={classToUse}>{children}</div>;
+    }
+  
+}
+
+function GenericList<T>({ items,  Component,  itemId }: ListProps<T>) {
   return (
-    <Wrapper>
-      {items.map((item, index) => (
-        <li key={index}>{renderItem(item)}</li>
+    <Wrapper  >
+      {items.map((item) => (
+        <Component key={itemId(item)} item={item} />
       ))}
     </Wrapper>
   );
